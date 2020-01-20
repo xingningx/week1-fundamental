002e13c2d3e51028316920fdb8ad9c81edb01862
xingningxu <59849663+xingningxu@users.noreply.github.com>
2020-01-20 17:12:41 -0500

    github new

--- src/testgithub/TestGithub.java
+++ src/testgithub/TestGithub.java
@@ -17,7 +17,7 @@ public class TestGithub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Git push operation test");
-        int newInt;
+        int newInt;// github version
         
     }
     
